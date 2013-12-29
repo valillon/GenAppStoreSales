70ac054ee0fa6b09c0b6ac00f177e21b131cec6a
valillon <valillon@gmail.com>
2013-12-29 21:06:41 +0100

    Update License in files and comments

--- GenAppStoreSales.java
+++ GenAppStoreSales.java
@@ -1,46 +1,27 @@
-/* Modified to download Apple Store rates.txt and plot it scheduled graphics
+/* Download scheduled Apple Store reports and generates charts.
  * Rafael Redondo Tejedor April 2013
- */
+ 
+The MIT License (MIT)
 
-/* ===========================================================
- * JFreeChart : a free chart library for the Java(tm) platform
- * ===========================================================
- *
- * (C) Copyright 2000-2011, by Object Refinery Limited and Contributors.
- *
- * Project Info:  http://www.jfree.org/jfreechart/index.html
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
- * License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
- * Other names may be trademarks of their respective owners.]
- *
- * ------------------------------
- * CombinedCategoryPlotDemo1.java
- * ------------------------------
- * (C) Copyright 2008, by Object Refinery Limited and Contributors.
- *
- * Original Author:  David Gilbert (for Object Refinery Limited);
- * Contributor(s):   ;
- *
- * Changes
- * -------
- * 05-May-2008 : Version 1 (DG);
- *
- */
+Copyright (c) 2013 valillon
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 
 import java.awt.Font;
@@ -99,7 +80,7 @@ public class GenAppStoreSales extends ApplicationFrame {
 	public static String email = "my@email.com"; 	 // Put your email account here
 	public static String password = "myPassword"; 	 // Put your itunes password here
     public static String autoingestionPreArgs = " " + email + " " + password + " " + appID + " Sales ";
-    public static String appSKU = "myAppSKU";
+    public static String appSKU = "mySKU"; 			 // Put your product SKU here 
 
 	public static String chartTitle, windowTitle;
 	public static int periodUnits;
